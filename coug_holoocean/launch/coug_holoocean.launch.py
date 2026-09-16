a7d03e479228306ec5b7e471cf5b4d3cf149a81f
Nelson Durrant <snelsondurrant@gmail.com>
2026-09-16 15:13:33 -0600

    Fix scenario file support

--- coug_holoocean/launch/coug_holoocean.launch.py
+++ coug_holoocean/launch/coug_holoocean.launch.py
@@ -45,7 +45,9 @@ def generate_launch_description() -> LaunchDescription:
             [agent_ns, "_params.yaml"],
         ]
     )
-    scenario_param_file = LaunchConfiguration("scenario_param_file")
+    scenario_param_file = PythonExpression(
+        ["'", LaunchConfiguration("scenario_param_file"), "' or '", agent_param_file, "'"]
+    )
 
     depth_camera_optical_frame = agent_frame(agent_ns, "depth_camera_optical_link")
     depth_link_frame = agent_frame(agent_ns, "depth_link")
@@ -76,7 +78,7 @@ def generate_launch_description() -> LaunchDescription:
             ),
             DeclareLaunchArgument(
                 "scenario_param_file",
-                default_value=agent_param_file,
+                default_value="",
             ),
             DeclareLaunchArgument(
                 "add_noise",
